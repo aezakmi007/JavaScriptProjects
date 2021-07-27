06620cdc230a2a75e5bcb1335c996ceb2662d6ba
aezakmi007 <abdullahfarhan910@gmail.com>
2021-07-27 21:32:26 +0530

    Number Guessing Game Partially Complted

---
+++ NumberGuessingGame/script.js
@@ -0,0 +1,66 @@
+'use strict';
+
+// document.querySelector('.message').textContent;
+
+// document.querySelector('.number').textContent = 13;
+//
+
+// document.querySelector('.guess').value = 23;
+let number = 0;
+let guess = 0;
+const generateRandom = () => {
+  number = Math.trunc(Math.random() * 20) + 1;
+};
+generateRandom();
+
+let score = 20;
+
+document.querySelector('.number').textContent = number;
+
+document.querySelector('.check').addEventListener('click', function () {
+  guess = Number(document.querySelector('.guess').value);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No Number';
+  } else if (guess === number) {
+    document.querySelector('.message').textContent = 'Correct Number';
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+  } else if (guess > number) {
+    if (score > 1) {
+      score = score - 1;
+      document.querySelector('.score').textContent = score;
+      document.querySelector('.message').textContent = 'Too High';
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game';
+      document.querySelector('.score').textContent = 0;
+      document.querySelector('.guess').value = '';
+    }
+  } else {
+    if (score > 1) {
+      score = score - 1;
+      document.querySelector('.score').textContent = score;
+      document.querySelector('.message').textContent = 'Too Low';
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game';
+      document.querySelector('.score').textContent = 0;
+      document.querySelector('.guess').value = '';
+    }
+  }
+});
+
+//game reset functionality
+
+// This function will only be called when click event happen
+
+document.querySelector('.again').addEventListener('click', () => {
+  if ((score != 1 && number === guess) || (score === 1 && number != guess)) {
+    score = 20;
+    generateRandom();
+    document.querySelector('.message').textContent = 'Guess the number';
+    document.querySelector('.score').textContent = score;
+    document.querySelector('.number').textContent = number;
+    document.querySelector('.guess').value = '';
+    document.querySelector('body').style.backgroundColor = 'black';
+  }
+});
